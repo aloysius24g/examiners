dd30e685f5c9057b72ddb3ba9d15e8c101b1fec8
a <>
2026-08-03 14:36:51 +0530

    added everything to cors policy just to see it work on temp tunnel. cnage later

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,9 @@ import rateLimit from "express-rate-limit";
 
 const corsOptions = {
   origin: [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://localhost:80',
+    '*',
   ],
   credentials: true,
 }
