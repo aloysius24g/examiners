c3e004a1e06f5a57112f525a02eb629462ecfd4c
a <>
2026-09-04 17:53:47 +0530

    added older browser combatibility, not so neat but fiexed somewhat

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,22 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { browserslistToTargets } from "lightningcss"
+import browserslist from 'browserslist';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%, not dead')),
+    },
+  },
+  build: {
+    target: 'es2020', 
+    cssTarget: 'chrome61',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
